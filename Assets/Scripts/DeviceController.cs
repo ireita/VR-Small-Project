857c90cf4c59e3af10696189543bcaf280e32504
Pavoex <61478958+Pavoex@users.noreply.github.com>
2021-01-02 21:18:05 +0100

    Primary2DAxis
    Fixed Primary2DAxis for WMR

---
+++ Assets/Scripts/DeviceController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.XR;
+
+public class DeviceController : MonoBehaviour
+{
+    [SerializeField]
+    private XRNode xrNode = XRNode.LeftHand;
+
+    private List<InputDevice> devices = new List<InputDevice>();
+
+    private InputDevice device;
+
+  
+   
+    void GetDevice()
+    {
+        InputDevices.GetDevicesAtXRNode(xrNode, devices);
+        device = devices.FirstOrDefault();
+    }
+
+    void OnEnable()
+    {
+        if (!device.isValid)
+        {
+            GetDevice();
+        }
+        
+    }
+    void Update()
+    {
+        if (!device.isValid)
+        {
+            GetDevice();
+        }
+
+        List<InputFeatureUsage> features = new List<InputFeatureUsage>();
+        device.TryGetFeatureUsages(features);
+
+        foreach (var feature in features)
+        {
+            Debug.Log(device.name + feature.name + feature.type);
+        }
+
+        
+    }
+}
