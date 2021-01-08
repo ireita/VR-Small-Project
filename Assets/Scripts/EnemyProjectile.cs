653290e45386e1aaf14c810503d114d325aefff4
Pavoex <61478958+Pavoex@users.noreply.github.com>
2021-01-09 00:43:02 +0100

    EnemyShoot and Movement

---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    [SerializeField]
+    float damage = 10f;
+
+    Rigidbody rb;
+
+    [SerializeField]
+    float speed = 500f;
+
+
+    private void Awake()
+    {
+        rb.GetComponent<Rigidbody>();
+        Transform target = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 direction = target.position - transform.position;
+        rb.AddForce(direction * speed * Time.deltaTime);
+    }
+
+    
+}
---
+++ Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject projectile;
+
+    Transform target;
+
+    [SerializeField]
+    Transform shootPoint;
+
+    [SerializeField]
+    float turnSpeed = 5f;
+    float firerate = 0.2f;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void Update()
+    {
+        firerate -= Time.deltaTime;
+
+        Vector3 direction =  transform.position - target.position ;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed * Time.deltaTime);
+
+        if (firerate<=0)
+        {
+            firerate = 0.2f;
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        Instantiate(projectile, shootPoint.position, shootPoint.rotation);
+    }
+}
